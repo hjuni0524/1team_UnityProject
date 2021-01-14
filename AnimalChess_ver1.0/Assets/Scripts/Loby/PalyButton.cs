4e7b86e54aa9a09bdacacfab03047e21554ec234
KingChanwoo <77060802+KingChanwoo@users.noreply.github.com>
2021-01-15 08:54:32 +0900

    UnityProject

---
+++ AnimalChess_ver1.0/Assets/Scripts/Loby/PalyButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PalyButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Play()
+    {
+
+    }
+    void UnPlay()
+    {
+
+    }
+
+
+
+}
