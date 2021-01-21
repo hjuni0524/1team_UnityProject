134448fcb1b1a033d68b3720c4a93e0d1cfde9b1
KingChanwoo <77060802+KingChanwoo@users.noreply.github.com>
2021-01-22 00:46:52 +0900

    Round update

---
+++ Battle/Assets/asoliddev - Auto Chess/Scripts/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stores basic Game Data
+/// </summary>
+public class GameData : MonoBehaviour
+{
+    ///Store all available champion, all champions must be assigned from the Editor to the Script GameObject
+    public Champion[] championsArray;
+
+    ///Store all available championType, all championTypes must be assigned from the Editor to the Script GameObject
+    public ChampionType[] championTypesArray;
+
+    //  private void Awake()
+    //  {
+    //      for (int i = 0; i < championsArray.Length; i++)
+    //      {
+    //          Debug.Log(championsArray[i].uiname);
+    //      }
+    //  }
+}
---
+++ Battle/Assets/asoliddev - Auto Chess/Scripts/Stage1.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1 : MonoBehaviour
+{
+    public AIopponent aiOpponent;
+
+
+    public void Round(int r)
+    {
+        if (r == 1)
+        {
+            // AddEnemy( 유닛ID, x좌표, z좌표)
+            // 아래 형식 복사해서 값만 바꿔서 사용
+            // 원하는 유닛 수 만큼 복사해서 사용
+            aiOpponent.AddEnemy(9, 1, 2);
+            aiOpponent.AddEnemy(9, 5, 2);
+        }
+        else if (r == 2)
+        {
+            aiOpponent.AddEnemy(9, 1, 2);
+            aiOpponent.AddEnemy(9, 5, 2);
+            aiOpponent.AddEnemy(9, 3, 1);
+        }
+        else if (r == 3)
+        {
+
+        }
+        else if (r == 4)
+        {
+
+        }
+        else if (r == 5)
+        {
+
+        }
+        else if (r == 6)
+        {
+
+        }
+        else if (r == 7)
+        {
+
+        }
+        else if (r == 8)
+        {
+
+        }
+        else if (r == 9)
+        {
+
+        }
+        else if (r == 10)
+        {
+
+        }
+        else if (r == 11)
+        {
+
+        }
+        else if (r == 12)
+        {
+
+        }
+        else if (r == 13)
+        {
+
+        }
+        else if (r == 14)
+        {
+
+        }
+        else if (r == 15)
+        {
+
+        }
+        else if (r == 16)
+        {
+
+        }
+        else if (r == 17)
+        {
+
+        }
+        else if (r == 18)
+        {
+
+        }
+        else if (r == 19)
+        {
+
+        }
+        else if (r == 20)
+        {
+
+        }
+        else if (r == 21)
+        {
+
+        }
+        else if (r == 22)
+        {
+
+        }
+        else if (r == 23)
+        {
+
+        }
+        else if (r == 24)
+        {
+
+        }
+        else if (r == 25)
+        {
+
+        }
+        else if (r == 26)
+        {
+
+        }
+    }
+    
+    
+    //public void round1(int n,)
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiOpponent = GameObject.Find("Scripts").GetComponent<AIopponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
